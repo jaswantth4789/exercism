410ec0db6704b4e1e82e96fe06d49d54d5373f66
jaswantth <potlurivenkatajaswantth@gmail.com>
2024-01-05 11:49:02 +0530

    added calculator

---
+++ calculator-conundrum/src/main/java/CalculatorConundrum.java
@@ -0,0 +1,26 @@
+class CalculatorConundrum {
+    public String calculate(int operand1, int operand2, String operation) {
+
+        if (operation == null) {
+            throw new IllegalArgumentException("Operation cannot be null");
+        }
+        
+        try {
+            switch (operation) {
+                case "+":
+                    return operand1 + " + " + operand2 + " = " + (operand1 + operand2);
+                case "*":
+                    return operand1 + " * " + operand2 + " = " + (operand1 * operand2);
+                case "/":
+                    return operand1 + " / " + operand2 + " = " + (operand1 / operand2);
+                case "":
+                    throw new IllegalArgumentException("Operation cannot be empty");
+                default:
+                    throw new IllegalOperationException("Operation '" + operation + "' does not exist");
+            }
+        }catch (ArithmeticException e){
+            throw new IllegalOperationException("Division by zero is not allowed",e);
+        }
+    }
+}
+
