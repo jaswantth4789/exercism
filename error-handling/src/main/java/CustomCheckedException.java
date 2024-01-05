af21fbe6675ce60417395ae96aaa706662cd7777
jaswantth <potlurivenkatajaswantth@gmail.com>
2024-01-05 11:53:43 +0530

    error handling

---
+++ error-handling/src/main/java/CustomCheckedException.java
@@ -0,0 +1,15 @@
+class CustomCheckedException extends Exception {
+
+    CustomCheckedException() {
+        super();
+    }
+
+    CustomCheckedException(String message) {
+        super(message);
+    }
+
+}
+
+
+
+
